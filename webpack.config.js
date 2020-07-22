df7bc9ef39b80cb49d88ef7b8d4296fa94eff6a7
Peyton Perret <kpeyton95@gmail.com>
2020-07-22 11:40:52 -0500

    create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist/public');
+
+module.exports = {
+  entry: [`${SRC_DIR}/index.js`],
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: SRC_DIR,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.png', '.jpg'],
+  },
+};
