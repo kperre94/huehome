6f3d5a661197994e06768f8684523f7a9f8fe140
Peyton Perret <kpeyton95@gmail.com>
2020-07-22 11:56:10 -0500

    create api

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, "../client/dist/public/")));
+app.use(express.json());
+// ROUTES
+
+// SERVER
+app.listen(process.env.PORT, () => {
+  console.log(`Ready on port: ${process.env.PORT}`);
+})
