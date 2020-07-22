7f561760034eb4e2e1a3b2e9bfabe5355a666190
Peyton Perret <kpeyton95@gmail.com>
2020-07-22 15:13:37 -0500

    functionality to get current device type for phillips hue

--- server/index.js
+++ server/index.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
+const device = require('express-device');
 const app = express();
 const db = require('./controllers/database');
 
 // MIDDLEWARE
 app.use(express.static(path.join(__dirname, "../client/dist/public/")));
 app.use(express.json());
+app.use(device.capture());
 // ROUTES
+
+app.get('/device', (req, res) => {
+  res.send(`Hi ` + req.device.type.toUpperCase()+ `User`);
+});
 
 // SERVER
 app.listen(process.env.PORT, () => {
