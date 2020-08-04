eaf1cb17e4b84e2c81ec46e41877ccad93bf971d
Peyton Perret <kpeyton95@gmail.com>
2020-08-04 18:36:01 -0500

    render login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
 
 class Login extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        user: {}
+        user: {
+            name: '',
+            email: '',
+            password: '',
+            location: '',
+        },
+        errorMsg:'',
+        emailError: ''
       }
       this.handleChange = this.handleChange.bind(this);
   }
@@ -13,18 +22,45 @@ class Login extends Component {
     let user = this.state.user;
     user[field] = e.target.value;
     this.setState({ user });
+    console.log(this.state.user)
   }
 
   render() { 
     return (
       <>
-        <div>
-          <form>
-            <label>Email</label>
-            <input type="email" onChange={(e) => this.handleChange(e, 'email')}></input>
-            <label>Password</label>
-            <input type="password" onChange={(e) => this.handleChange(e, 'password')}></input>
-          </form>
+        <div className="login">
+      <div className="grid center middle tall">
+          <div className="card small">
+            <div className="content">
+              <form>
+                {/* <div className="group">
+                  <label>Email</label>
+                  <input type="email" onChange={(e) => this.handleChange(e, 'email')}></input>
+                </div> */}
+                <div className="group">
+                  <label>Role</label>
+                  <select>
+                    <option>select one</option>
+                    <option>admin</option>
+                    <option>user</option>
+                  </select>
+                  <label>Name</label>
+                  <input type="password" onChange={(e) => this.handleChange(e, 'password')}></input>
+                </div>
+                <div className="group">
+                  <label>Password</label>
+                  <input type="password" onChange={(e) => this.handleChange(e, 'password')}></input>
+                </div>
+                <button className="button" onClick={() => onRouteChange('home')}>Login</button>
+              </form>
+              <span style={{color:"red"}}>{this.state.errorMsg}</span>
+              <p className="footer">
+                  If you haven't been assigned access, please see our Butler in the main entrance. 
+                  
+              </p>
+            </div>
+          </div>
+        </div>
         </div>
       </>
     );
