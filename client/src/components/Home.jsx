8a50d2784a2aeb477ac3698cc326bf87df8a80bf
Peyton Perret <kpeyton95@gmail.com>
2020-09-15 15:41:15 -0500

    able to control the state of any light

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import runtime from 'regenerator-runtime';
+import 'regenerator-runtime';
 import axios from 'axios';
+import Nav from './Nav';
 import Thumbnail from './Thumbnail';
-
+import { get } from '../lib/api.js';
 
 class Home extends Component {
   constructor(props) {
@@ -13,18 +14,22 @@ class Home extends Component {
           avatar: ''
         },
         userLights: [],
+        locations: [],
+        weather: {},
+        colorpicker: false
       }
       this.getLight = this.getLight.bind(this);
       this.updateLight = this.updateLight.bind(this);
-      this.alert = this.alert.bind(this);
+      this.toggleColorpicker = this.toggleColorpicker.bind(this);
+      this.getLocations = this.getLocations.bind(this);
   }
 
   async getLight(e, i, index) {
     try {
       let state = !this.state.userLights[i][1].state.on;
-      let light = await axios.get(`/lights/${index}`, { on: state})
+      let light = await get (`/lights/${index}`, { on: state})
+      console.log(light)
       this.setState({ light });
-
     } catch (e) { console.log(e)};
   }
 
@@ -32,7 +37,7 @@ class Home extends Component {
     try {
       let userLights = this.state.userLights;
       let state = !this.state.userLights[i][1].state.on;
-      let response = await axios.put(`/lights/${index}`, { on: state });
+      let response = await post (`/lights/${index}`, { on: state });
       let hue = response.data[0].success[`/lights/${index}/state/on`]
       let currentLight = userLights[i][1];
       currentLight.state.on = hue;
@@ -41,10 +46,13 @@ class Home extends Component {
     } catch (e) { console.log(e)};
   }
 
-  async alert() {
-    try {
-      this.updateLight(7, 41)
-    } catch (e) { console.log(e)};
+  toggleColorpicker(i, index) {
+    let userLights = this.state.userLights;
+    let colorpicker = !this.state.userLights[i][1].colorpicker;
+    let currentLight = userLights[i][1];
+    currentLight['colorpicker'] = colorpicker;
+    this.setState({ userLights });
+    console.log(this.state.userLights[i][1]['colorpicker'])
   }
 
   async componentDidMount() {
@@ -53,32 +61,45 @@ class Home extends Component {
         let userLights = []
         let obj = lights['data'];
           for (let key in obj) {
-            userLights.push([key, obj[key]]);
+            userLights.push([key, obj[key], obj[key].colorpicker = false]);
           }
           this.setState({ userLights });
+          console.log(this.state.userLights)
     } catch (e) { console.log(e)};
+  }
+
+    async getLocations() {
+    try {
+      let data = await axios.get('/locations')
+      let locations = data.json();
+      this.setState({ locations });
+    } catch (e) { console.log(e)}
   }
 
   render() { 
     return (
       <>
           <div className="grid tall image home">
+            <Nav />
             <div className="grid middle five large">
               {this.state.userLights.map((l, i) => { 
                 return ( 
-                  <Thumbnail key={i}
-                            light={l}
-                            index={i}
-                            getLight={this.getLight}
-                            updateLight={this.updateLight}
-                            onClick={() => onRouteChange('light')} />
+                  <Thumbnail 
+                    key={i}
+                    light={l}
+                    index={i}
+                    i={i}
+                    getLight={this.getLight}
+                    updateLight={this.updateLight}
+                    toggleColorpicker={this.toggleColorpicker}
+                  />
                 )
               })}
             </div>
           </div>
       </>
-    );
+    )
   }
-}
+};
  
 export default Home;
