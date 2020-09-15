ba12e30e6e2e86413b62dc734ee641c2b36a842e
Peyton Perret <kpeyton95@gmail.com>
2020-09-15 15:45:51 -0500

    implement correct API routes and implemented seperation of concerns

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
-const db = require('./controllers/database');
 const cors = require('cors');
 const axios = require('axios');
+const jwt = require('express-jwt');
+// const auth = jwt({ secret: process.env.JWT_SECRET })
 
-const host = "http://192.168.86.73";
-const username = "kTbxhpG9ZObv7b7ZTJ7Ytu0dCro6cZuey88an1y5";
+const { host, username } = require('../config');
 const hueUrl = `${host}/api/${username}`;
 
 const app = express();
@@ -17,12 +17,16 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "../client/dist/public/")));
 
 // ROUTES
+app.post('/signup', require('./controllers/postSignup'));
+app.post('/signin', require('./controllers/postSignin'));
+
 app.get('/lights', async (req, res) => {
   try {
     let lights = await axios.get(`${hueUrl}/lights`)
     res.json(lights.data);
   } catch (e) { console.log(e) };
 })
+
 app.get(`/lights/:id`, async (req, res) => {
   try {
     console.log(req.params)
@@ -32,6 +36,14 @@ app.get(`/lights/:id`, async (req, res) => {
   } catch (e) { console.error(e) };
 })
 
+app.post(`/lights/:id`, async (req, res) => {
+  try {
+    let options = req.body;
+    let id = req.params.id
+    let response = await axios.put(`${hueUrl}/lights/${id}/state`, options);
+    res.json(response.data)
+  } catch (e) { console.error(e) };
+})
 
 // SERVER
 app.listen(process.env.PORT, () => {
